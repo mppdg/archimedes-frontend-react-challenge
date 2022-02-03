9b40af9232517821914c63fe66557c83cc946e90
Mapopo (Evans) <mppdgreat@gmail.com>
2022-02-03 23:59:36 +0100

    Control form input

--- src/components/common/UserForm.jsx
+++ src/components/common/UserForm.jsx
@@ -1,21 +1,32 @@
+import { useState } from "react";
 import ActionButton from "./ActionButton";
 
-const UserForm = ({ actionText }) => {
+const UserForm = ({ actionText, data = { name: "", email: "" } }) => {
+    const [user, setUser] = useState(data);
+
+    const handleChange = (e) => {
+        setUser({
+            ...user,
+            [e.target.id]: e.target.value
+        })
+    }
+
     return (
         <form className="user-form">
             <div className="form-group">
                 <label htmlFor="name">Name</label>
-                <input type="text" id="name" name="name" placeholder="Your name" />
+                <input type="text" id="name" value={user.name} placeholder="Your name" onChange={handleChange} />
             </div>
             <div className="form-group">
                 <label htmlFor="email">Email</label>
-                <input type="text" id="email" name="email" placeholder="Your email" />
+                <input type="text" id="email" value={user.email} placeholder="Your email" onChange={handleChange} />
             </div>
             <div className="form-group">
                 <label htmlFor="role">Role</label>
-                <select id="role" name="role">
-                    <option value="user">User</option>
-                    <option value="admin">Admin</option>
+                <select id="role" name="role" placeholder="Your email" onChange={handleChange}>
+                    <option disabled>Select user role</option>
+                    <option value="User" selected={user.role === "User"}>User</option>
+                    <option value="Admin" selected={user.role === "Admin"}>Admin</option>
                 </select>
             </div>
             <div className="form-group" style={{ marginBottom: "3px" }}>
