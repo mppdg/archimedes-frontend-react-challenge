baac42468c4d62f311a90d833104ae1b25aaaca6
Mapopo (Evans) <mppdgreat@gmail.com>
2022-02-03 22:47:53 +0100

    Allow navigation from update view to list view with the arrow back button

--- src/components/views/UpdateUser.jsx
+++ src/components/views/UpdateUser.jsx
@@ -1,5 +1,6 @@
 import { MdKeyboardBackspace } from "react-icons/md";
 import { useHistory } from "react-router-dom";
+import { USERS_ROUTE } from "../../constants";
 import ContentCard from "../common/ContentCard";
 import UserForm from "../common/UserForm";
 
@@ -8,17 +9,20 @@ import UserForm from "../common/UserForm";
  * Display and update user profile
  */
 const UpdateUser = () => {
-
     const history = useHistory();
 
-    const handleClick = () => history.push('/users')
+    const title = (
+        <span>
+            <span onClick={() => history.push(USERS_ROUTE)}><MdKeyboardBackspace /></span>
+            <strong>Update User</strong>
+        </span>
+    )
 
-    const title = <span><span onClick={}><MdKeyboardBackspace /></span> <strong>Update User</strong></span>
     const content = <UserForm actionText="Update User" />
-    
-      return (
+
+    return (
         <ContentCard content={content} title={title} />
-      );
+    );
 };
 
 export default UpdateUser;
